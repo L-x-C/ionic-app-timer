d2249f7293ec1980e67f079ccb75d5fa673ad4b5
L-x-C <lxc.neal@gmail.com>
2015-05-26 17:45:29 +0800

    style

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -8,10 +8,9 @@
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+	$ionicPlatform.ready(function() {
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
 })
-
--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -1,40 +1,56 @@
 angular.module('starter.controllers', [])
+	.controller('ToDoListCtrl', function($scope, $ionicModal, $interval) {
+		$ionicModal.fromTemplateUrl('modal.html', {
+			scope: $scope,
+			animation: 'slide-in-up'
+		}).then(function(modal) {
+			$scope.modal = modal;
+		});
 
-.controller('ToDoListCtrl', function($scope, $ionicModal) {
-    $scope.toDoListItems = [{
-        task: 'Scuba Diving',
-        status: 'not done'
-    }, {
-        task: 'Climb Everest',
-        status: 'not done'
-    }];
+		$scope.data = {
+			firstRound: 5,
+			secondRound: 6,
+			time: 5
+		};
 
-    $ionicModal.fromTemplateUrl('modal.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-    }).then(function(modal) {
-        $scope.modal = modal;
-    });
+		$scope.openModal = function() {
+			$scope.modal.show();
+		};
 
-    $scope.openModal = function() {
-        $scope.modal.show();
-    };
+		$scope.closeModal = function() {
+			$scope.modal.hide();
+		};
 
-    $scope.closeModal = function() {
-        $scope.modal.hide();
-    };
+		$scope.$on('$destroy', function() {
+			$scope.modal.remove();
+		});
 
-    $scope.$on('$destroy', function() {
-        $scope.modal.remove();
-    });
+		$scope.startTimer = function() {
+			var flag = true,
+				ft = $scope.data.firstRound,
+				st = $scope.data.secondRound;
+			var firstRoundFn = function() {
+				$scope.data.time = --ft;
+				if ($scope.data.time <= 0) {
+					flag = false;
+					ft = $scope.data.firstRound;
+				}
 
-    $scope.AddItem = function(data) {
-        $scope.toDoListItems.push({
-            task: data.newItem,
-            status: 'not done'
-        });
+			};
+			var secondRoundFn = function() {
+				$scope.data.time = --st;
+				if ($scope.data.time <= 0) {
+					flag = true;
+					st = $scope.data.secondRound;
+				}
+			};
 
-        data.newItem = ' ';
-        $scope.closeModal();
-    }
-});
+			$interval(function() {
+				if (flag) {
+					firstRoundFn();
+				} else {
+					secondRoundFn();
+				}
+			}, 1000);
+		}
+	});
